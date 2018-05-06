4b6b52f952c582df1d2f28e469707b5225ec714d
Irina Smirnova <smirnovapr@mail.ru>
2018-05-06 15:25:54 +0300

    Create gh-page

--- src/helpers/storage-helper.js
+++
@@ -1,6 +0,0 @@
-export const pullLikesFromStorage = (array) => {
-  const items = JSON.parse(localStorage.getItem('likes')) || [];
-  const isLiked = id => !!items.find(item => item === id);
-
-  return array.map(item => ({ ...item, isLiked: isLiked(item.id) }));
-};
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-import productsReducer from './products-reducer';
-
-export default combineReducers({
-  products: productsReducer,
-  routing: routerReducer
-});
